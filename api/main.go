f1df0142b09b3692dd99ec3b82363a8d3f0f989e
Elías Ramírez Hernández <eliasramirezhernandez1@gmail.com>
2026-09-06 17:54:46 -0600

    refactor: move files to correct dirs
    create basic project structure
    
    Co-Authored-By: MatiasBS027 <153869123+MatiasBS027@users.noreply.github.com>
    Co-Authored-By: Crisbor77 <206800107+Crisbor77@users.noreply.github.com>

--- api/main.go
+++ api/main.go
@@ -4,11 +4,21 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func reservas(c *gin.Context) {
+	// TODO: insert en reservas
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/", func(c *gin.Context) { // home
 		c.JSON(200, gin.H{
 			"message": "Welcome to My Website",
+		})
+	})
+
+	router.POST("/reservas", func(c *gin.Context) { // reservas
+		c.JSON(200, gin.H{
+			"message": "Reserva created successfully",
 		})
 	})
 
